d3c7b5d9319c8fd5254522f823439884e8be940e
numq <numq.dev@gmail.com>
2025-01-03 01:09:56 +0300

    Add library module

---
+++ library/src/main/kotlin/com/github/numq/stretch/NativeStretch.kt
@@ -0,0 +1,63 @@
+package com.github.numq.stretch
+
+import java.lang.ref.Cleaner
+
+internal class NativeStretch : AutoCloseable {
+    private val nativeHandle = initNative().also { handle ->
+        require(handle != -1L) { "Unable to initialize native library" }
+    }
+
+    private val cleanable = cleaner.register(this) { freeNative(nativeHandle) }
+
+    private companion object {
+        val cleaner: Cleaner = Cleaner.create()
+
+        @JvmStatic
+        external fun initNative(): Long
+
+        @JvmStatic
+        external fun getInputLatencyNative(handle: Long): Int
+
+        @JvmStatic
+        external fun getOutputLatencyNative(handle: Long): Int
+
+        @JvmStatic
+        external fun configureNative(handle: Long, channels: Int, blockSamples: Int, intervalSamples: Int)
+
+        @JvmStatic
+        external fun processNative(
+            handle: Long,
+            pcmBytes: ByteArray,
+            channels: Int,
+            playbackSpeedFactor: Float,
+        ): ByteArray
+
+        @JvmStatic
+        external fun resetNative(handle: Long)
+
+        @JvmStatic
+        external fun freeNative(handle: Long)
+    }
+
+    fun getInputLatency() = getInputLatencyNative(handle = nativeHandle)
+
+    fun getOutputLatency() = getOutputLatencyNative(handle = nativeHandle)
+
+    fun configure(channels: Int, blockSamples: Int, intervalSamples: Int) = configureNative(
+        handle = nativeHandle,
+        channels = channels,
+        blockSamples = blockSamples,
+        intervalSamples = intervalSamples
+    )
+
+    fun process(pcmBytes: ByteArray, channels: Int, playbackSpeedFactor: Float) = processNative(
+        handle = nativeHandle,
+        pcmBytes = pcmBytes,
+        channels = channels,
+        playbackSpeedFactor = playbackSpeedFactor
+    )
+
+    fun reset() = resetNative(handle = nativeHandle)
+
+    override fun close() = cleanable.clean()
+}
---
+++ library/src/main/kotlin/com/github/numq/stretch/Stretch.kt
@@ -0,0 +1,45 @@
+package com.github.numq.stretch
+
+interface Stretch : AutoCloseable {
+    val sampleRate: Int
+    val channels: Int
+    val playbackSpeedFactor: Float
+    suspend fun changePlaybackSpeedFactor(factor: Float): Result<Unit>
+    suspend fun getInputLatency(): Result<Int>
+    suspend fun getOutputLatency(): Result<Int>
+    suspend fun process(pcmBytes: ByteArray): Result<ByteArray>
+    suspend fun reset(): Result<Unit>
+
+    companion object {
+        private var isLoaded = false
+
+        /**
+         * Loads the native library.
+         *
+         * @param libstretch the path to the libstretch library.
+         * @return a [Result] indicating the success or failure of the operation.
+         */
+        fun load(libstretch: String) = runCatching {
+            System.load(libstretch)
+        }.onSuccess {
+            isLoaded = true
+        }
+
+        /**
+         * Creates a new instance of [Stretch].
+         *
+         * @return a [Result] containing the created instance if successful.
+         * @throws IllegalStateException if the native libraries are not initialized or if there is an issue with the underlying native library.
+         */
+        fun create(sampleRate: Int, channels: Int, defaultPlaybackSpeedFactor: Float): Result<Stretch> = runCatching {
+            check(isLoaded) { "Native binaries were not loaded" }
+
+            DefaultStretch(
+                sampleRate = sampleRate,
+                channels = channels,
+                playbackSpeedFactor = defaultPlaybackSpeedFactor,
+                nativeStretch = NativeStretch()
+            )
+        }
+    }
+}
